3a6f1b4baa9ca3869b99c252a25ea7a2f89d923e
sometime1992 <sometime1992@naver.com>
2024-10-28 11:49:36 +0900

    20241028

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+// variavle: 상대 경로 상수//
+// - 출력되는 화면에 pathVariable (동적인 데이터값)이 필요 없는 경우: 상수(변수)
+// - 출력되는 화면에 pathVariable(동적인 데이터값)이 필요 없는 경우
+//    : 매개변수로 데이터값을 받아 템플릿 리터럴로 명시하는 함수
+
+export const MAIN_PATH = '/';
+
+export const AUTH_PATH = '/auth';
+
+export const BOARD_DETAIL_PATH = (boardNumber: number | string) => `/board/detail/${boardNumber}`;
+export const BOARD_WRITE_PATH = '/board/write';
+export const BOARD_UPDATEL_PATH = (boardNumber: number | string) => `/board/detail/${boardNumber}`;
+export const USER_PATH = '/user';
+
+export const REACT_STUDY_PATH = '/react-study';
---
+++ src/react-study/기초.ts
@@ -0,0 +1,19 @@
+export const tmp = "";
+/* 
+
+# <<< 단축키.ts >>> 
+
+- 터미널 열기: Ctrl + Shift + `(백틱)
+
+- 줄 복사: Alt + Shift + 방향키(위/아래)
+- 줄 이동: Alt + 방향키(위/아래)
+
+- 들여쓰기: tab
+- 내어쓰기: Shift + tab
+
+- 사이드바 토글: Ctrl + b
+
+- 검색: Ctrl + f
+
+*/
+
